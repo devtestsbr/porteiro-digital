0b02296e157df70f5c56c1227b138dd1acf0543a
Bruno <brunonoriller@gmail.com>
2023-04-09 10:13:40 -0300

    refactoring tests

--- src/Home/Home.spec.tsx
+++ src/Home/Home.spec.tsx
@@ -2,36 +2,36 @@ import { locations } from '@/mockGen';
 import { mockFetchResolve, render, screen, waitFor } from '@/test/utils';
 import { Home } from '.';
 
-describe('<Location>', () => {
+describe('<Home>', () => {
   const mock: locations = [
     ['mockId', 'mockName'],
     ['mockId2', 'mockName2'],
   ];
 
-  beforeEach(() => {
+  beforeEach(async () => {
     mockFetchResolve(mock);
 
     render(<Home />);
+
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
   });
 
   it('renders the page heading', async () => {
-    await waitFor(() => {
-      screen.getByRole('heading', { name: 'Locations' });
-    });
+    expect(
+      screen.getByRole('heading', { name: 'Locations' }),
+    ).toBeInTheDocument();
   });
 
   it('renders two location buttons', async () => {
-    await waitFor(() => {
-      expect(screen.getAllByRole('link')).toHaveLength(mock.length);
-    });
+    expect(screen.getAllByRole('link')).toHaveLength(mock.length);
   });
 
   it('renders a button with link', async () => {
-    await waitFor(() => {
-      expect(screen.getByRole('link', { name: mock[0][1] })).toHaveAttribute(
-        'href',
-        `/location/${mock[0][0]}`,
-      );
-    });
+    expect(screen.getByRole('link', { name: mock[0][1] })).toHaveAttribute(
+      'href',
+      `/location/${mock[0][0]}`,
+    );
   });
 });
--- src/Location/Location.spec.tsx
+++ src/Location/Location.spec.tsx
@@ -11,40 +11,36 @@ describe('<Location>', () => {
     ],
   };
 
-  beforeEach(() => {
+  beforeEach(async () => {
     mockFetchResolve(mock);
 
     render(<Location id={'1'} />);
+
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
   });
 
   it('renders the location name', async () => {
-    await waitFor(() => {
-      expect(
-        screen.getByRole('heading', { name: RegExp(mock.location) }),
-      ).toBeInTheDocument();
-    });
+    expect(
+      screen.getByRole('heading', { name: RegExp(mock.location) }),
+    ).toBeInTheDocument();
   });
 
   it('renders on apartment button', async () => {
-    await waitFor(() => {
-      const aptButton = screen.getByText(mock.apt[0][1]);
-      expect(aptButton).toBeInTheDocument();
-      expect(aptButton).toHaveAttribute('href', `/apt/${mock.apt[0][0]}`);
-    });
+    const aptButton = screen.getByText(mock.apt[0][1]);
+    expect(aptButton).toBeInTheDocument();
+    expect(aptButton).toHaveAttribute('href', `/apt/${mock.apt[0][0]}`);
   });
 
   it('renders the go back button', async () => {
-    await waitFor(() => {
-      expect(screen.getByRole('link', { name: /</ })).toBeInTheDocument();
-    });
+  expect(screen.getByRole('link', { name: /</ })).toBeInTheDocument();
   });
 
   it('renders two apartment buttons', async () => {
     const goBackButton = 1;
-    await waitFor(() => {
-      expect(screen.getAllByRole('link')).toHaveLength(
-        mock.apt.length + goBackButton,
-      );
-    });
+    expect(screen.getAllByRole('link')).toHaveLength(
+      mock.apt.length + goBackButton,
+    );
   });
 });
